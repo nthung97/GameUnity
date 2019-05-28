052b5aef2c23bdfe08b7643ce0be00785f72bc84
Thai Hung Nguyen <nthung@apcs.vn>
2019-05-28 10:42:57 +0700

    A02

---
+++ A02/Candy board/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour {
+
+    public GameObject[] candies; 
+	// Use this for initialization
+	void Start () {
+        Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Initialize()
+    {
+        int candyToUse = Random.Range(0, candies.Length);
+        GameObject candy = Instantiate(candies[candyToUse], transform.position, Quaternion.identity);
+        candy.transform.parent = this.transform;
+        candy.name = this.gameObject.name;
+    }
+}
