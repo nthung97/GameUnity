a2b3130f526a2b5a73d906c0aefd1572ab2fdb0e
Thai Hung Nguyen <nthung@apcs.vn>
2019-06-09 09:05:51 +0700

    A03

---
+++ A03/Control Mario/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    public float speed = 1.0f;
+    bool isOnGround = true;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+        }
+        if(Input.GetKey(KeyCode.Space) && isOnGround == true)
+        {
+            isOnGround = false;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 20 * speed));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+        if(collision.gameObject.name == "enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ A03/Control Mario/Assets/Scripts/KinematicControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicControl : MonoBehaviour {
+    Vector2 newPos;
+
+    public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+        newPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPos.x -= speed;
+
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            newPos.x += speed;
+        }
+        transform.position = newPos;
+	}
+}
